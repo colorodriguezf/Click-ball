b13e7f7f491d38ea0dff83942c697c5d1845a483
colorodriguezf <colitorodriguez97@gmail.com>
2023-08-24 17:37:34 -0300

    juego terminado

--- js/game.js
+++ js/game.js
@@ -1,76 +1,77 @@
 "use strict"
-document.addEventListener("DOMContentLoaded",restarTiempo);
+let btn_jguar =document.getElementById("btn-jugar");
+btn_jguar.addEventListener("click",jugar);
 document.getElementById("player").addEventListener("click", sumarPuntos);
 let puntos = 0;
-let tiempo= 60;
-
+let tiempo= 8;
+const puntuacion = document.getElementById("puntos");
+const puntosTotales = document.getElementById("puntosTotales");
 const audioClick = new Audio();
 audioClick.src= "/sound-effects/ES_Bubble Blip 2 - SFX Producer.mp3";
-console.log(audioClick);
+const body = document.body;
+
+const contenedor = document.querySelector('.contenedor');
+let ancho="";
+let alto = ""; 
+document.getElementById("player").style.display="none";
 
 function sumarPuntos() {
     puntos++;
-    document.getElementById("puntos").innerHTML="Puntos: <b> "+puntos+ "</b>";
-    document.getElementById("puntosTotales").innerHTML=puntos;
+    puntuacion.innerHTML="Puntos: <b> "+puntos+ "</b>";
+    puntosTotales.innerHTML=puntos;
     let colores =['#4357AD','#48a9a6','#c1666b','#8eecf5','#e63946','#03071e','#560bad','#ff0054', '#ff35c2','#e4fff9','#f4ff91',
-                        '#BDC3C7', '#5499C7', '#9C640C'];
+                '#BDC3C7', '#5499C7', '#9C640C'];
     let colorRandom = Math.round(Math.random()*colores.length);
     let randomColor= colores[colorRandom];
-    console.log(randomColor);
-        
+  
+    let random1 = Math.round(Math.random()*ancho);
+    let random2 = Math.round(Math.random()*alto);
 
-    
-    let random1 = Math.round(Math.random()*600);
-    let random2 = Math.round(Math.random()*520);
-    console.log(random1);
-    console.log(random2);
     document.getElementById("player").style.marginLeft =random1 + "px";
     document.getElementById("player").style.marginTop =random2 + "px";
     let a=document.getElementById("player").style.backgroundColor=randomColor;
-    console.log(a);
 }
 
+let interval = "";
+function jugar() {
+     ancho = contenedor.clientWidth - 20;
+     alto = contenedor.clientHeight; 
+    document.getElementById("player").style.display="flex";
+    puntosTotales.innerHTML = 0;
+    puntuacion.innerHTML = "Puntos:"+ 0;
+    puntos = 0;
+    tiempo = 8;
+    restarTiempo();
+    interval = setInterval(restarTiempo,1000);
+}
 
 function restarTiempo() {
-    tiempo--;
+    if(tiempo > 0) {
+        tiempo--;
+    }
     document.getElementById("tiempo").innerHTML="Tiempo: "+tiempo;
     if (tiempo == 0) {
-        // alert("GAME OVER, Tu punuacion fue de: " +puntos);
-        tiempo = 0;
         clearInterval(interval);
         document.getElementById("player").style.display="none";
         document.getElementById("modalPuntos").style.display="flex";
+        btn_jguar.disabled = true;
     }
 
 }
-// setInterVal -->mala practica
-let interval = setInterval(restarTiempo,1000);
 
+// const restartBoton = document.getElementById("restart");
 
-let API_URL= "api/puntaje";
-document.getElementById("btn-enviarPuntos").addEventListener("click",enviarPuntos);
+// restartBoton.addEventListener("click", () => {
+//   jugar();
+// });
 
-async function enviarPuntos() {
-    let usuario_fk = document.getElementById("data-usuario").value;
-    console.log(usuario_fk);
-    console.log(puntos);
+const btnModal = document.getElementById('btn-modal');
+const modal = document.getElementById('modalPuntos');
 
-    let puntuacionNueva = {
-        "usuario_fk":usuario_fk,
-        "puntos":puntos
-    }
-    try {
-        let response = await fetch(API_URL, { 
-            "method": "POST",
-            "headers": {"Content-Type": "application/json"},
-            "body": JSON.stringify(puntuacionNueva)
-        });
-        if (response.ok) {
-            console.log("Puntuacion de "+usuario_fk+ " añadida con exito");
-        }
-    }
-    catch (e) {
-        consol.log(e);
-    }
-    document.getElementById("modalPuntos").style.display="none";
+btnModal.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.style.display = 'none';
+    btn_jguar.disabled = false;
 }
+
